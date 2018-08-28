8b208d7dbf724cdc6824ea12151b4e78da5944dc
Joseph Chalupa <grammerking113@gmail.com>
2018-08-28 09:04:41 -0500

    Word Problem

---
+++ Excercises/src/chapter1Excercises/SammysMotto.java
@@ -0,0 +1,10 @@
+package chapter1Excercises;
+
+public class SammysMotto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sammy's makes it fun in the sun.");
+	}
+
+}
