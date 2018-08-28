59297951d88e22149ef3d296cb0eadafbe5e7820
Joseph Chalupa <grammerking113@gmail.com>
2018-08-28 08:58:21 -0500

    Bugs
    You had to import javax for it to work.

---
+++ Excercises/src/chapter1Excercises/DebugExcersise4.java
@@ -0,0 +1,10 @@
+package chapter1Excercises;
+import javax.swing.JOptionPane;
+public class DebugExcersise4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "First GUI program");
+	}
+
+}
